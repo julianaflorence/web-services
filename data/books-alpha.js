ce024f34e1188b3f9d748e382d27b3144ecfe810
julianaflorence <julianapuyallup@gmail.com>
2022-08-09 23:01:58 -0700

    services

---
+++ data/books-alpha.js
@@ -0,0 +1,97 @@
+"title": "Favorite Books",
+	
+"books":[
+          {
+    "Book":1,  
+            "Title":"A thousand Splendid Suns",
+            "Author":"Khaled Hosseini",
+            "Genre":"Fiction",
+            "Rating":"5/5",
+            "Description":"Two young women living in Afganistan cross paths in their troubled lives.",
+            "Image":"wrath-dawn.jpg"
+          },
+    {
+            "Book":2,
+            "Title":"Things Fall Apart",
+            "Author":"Chinuah Achebe",
+            "Genre":"Fiction",
+            "Rating":"4/5",
+            "Description":"A mans in the Igbo tribe of Nigeria experiences the colonisation of his land by the British",
+            "Image":"handmaids-tale.jpg"
+          },
+    {
+            "Book":3,
+            "Title":"All the Light we Cannot See",
+            "Author":"Anthony Doerr",
+            "Genre":"Historical Fiction",
+            "Rating":"4.5/5",
+            "Description":"A narrator follows a young boy in Nazi Germany and a young girl in occupied France and their adventure to find a legendary mystery",
+            "Image":"pride.jpg"
+          },
+    {
+            "Book":4,
+            "Title":"Eleanor Oliphant is Completely Fine",
+            "Author":"Gail Honeyman",
+            "Genre":"Romance",
+            "Rating":"3.7/5",
+            "Description":"A troubled 30-something year old living in the UK develops are para-social DList celeberty while struggling through past traumas and makes friends along the way",
+            "Image":"alone-w.jpg"
+    },
+        
+    {
+            "Book":5,
+            "Title":"It ends with us",
+            "Author":"Colleen Hoover",
+            "Genre":"Romance",
+            "Rating":"3.2/5",
+            "Description":"Sometimes it is the one who loves you who hurts you the most.",
+            "Image":"it-ends-w-us.jpg"
+          },
+    {
+            "Book":6,
+            "Title":"This Time Next Year",
+            "Author":"Sophie Cousens",
+            "Genre":"Romance",
+            "Rating":"4.5/5",
+            "Description":"A young man and women develop a friendship while slowing discovering they crossed each others paths their whole lives",
+            "Image":"neuro.jpg"
+          },
+    {
+            "Book":7,
+            "Title":"The careful undressing of love",
+            "Author":"Richelle Mead",
+            "Genre":"YA Scifi",
+            "Rating":"4.1/5",
+            "Description":"Lissa Dragomir is a Moroi princess: a mortal vampire with a rare gift for harnessing the earth's magic. She must be protected at all times from Strigoi; the fiercest vampires—the ones who never die.",
+            "Image":"vampire.jpg"
+          },
+    {
+            "Book":8,
+            "Title":"the invisible life of addie larue",
+            "Author":"V.E Schwab",
+            "Genre":"Scifi",
+            "Rating":"4.2/5",
+            "Description":"France, 1714: in a moment of desperation, a young woman makes a Faustian bargain to live forever and is cursed to be forgotten by everyone she meets. But everything changes when, after nearly 300 years, Addie stumbles across a young man in a hidden bookstore and he remembers her name.",
+            "Image":"invisible.jpg"
+          },
+    {
+            "Book":9,
+            "Title":"Vicious",
+            "Author":"V.E Schwab",
+            "Genre":"Scifi",
+            "Rating":"4.2/5",
+            "Description":"Victor and Eli started out as college roommates—brilliant, arrogant, lonely boys who recognized the same sharpness and ambition in each other. In their senior year, a shared research interest in adrenaline, near-death experiences, and seemingly supernatural events reveals an intriguing possibility: that under the right conditions, someone could develop extraordinary abilities. But when their thesis moves from the academic to the experimental, things go horribly wrong.",
+            "Image":"vicious.jpg"
+          },
+    {
+            "Book":10,
+            "Title":"Funny You Should Ask",
+            "Author":"Elissa Sussman",
+            "Genre":"Romance",
+            "Rating":"3.8/5",
+            "Description":"A restless young journalist with big dreams interviews a Hollywood heartthrob--and, ten years later, it's clear that their time together meant more than meets the eye in this sexy, engrossing adult debut novel.",
+            "Image":"funny-you.jpg"
+          }
+ 
+]
+}
