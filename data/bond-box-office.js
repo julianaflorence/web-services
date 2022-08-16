9dd4ae0fb332956442f6aabf30c289a5e384e1fc
Juliana Onwukwe <103076960+julianaflorence@users.noreply.github.com>
2022-08-16 07:48:02 +0000

    for now

--- data/bond-box-office.js
+++ data/bond-box-office.js
@@ -1,320 +1,90 @@
 {
-	"title": "Bond Films by Box Office Totals",
+	"title": "Books by Ranking",
 	
 	"films":[
-             {
-				"Film":23,
-				"Title":"Skyfall",
-				"Year":2012,
-				"Director":"Sam Mendes",
-				"Producers":"Michael G. Wilson and Barbara Broccoli",
-				"Writers":"Neal Purvis, Robert Wade and John Logan",
-				"Composer":"Thomas Newman",
-				"Bond":"Daniel Craig",
-				"Budget":"$230,000,000.00",
-				"BoxOffice":"$1,110,526,981.00",
-				"Image":"skyfall.jpg"
+        {
+				"Title": A Thousand Splendid Suns,
+				"Author":"Khaled Hosseini",
+				"Year":"2007",
+				"Pages": "384",
+        "Stars": 5,
+				"Image":"1958brazil.jpg"
 			  },
-              {
-				"Film":24,
-				"Title":"Spectre",
-				"Year":2015,
-				"Director":"Sam Mendes",
-				"Producers":"Michael G. Wilson and Barbara Broccoli",
-				"Writers":"Neal Purvis, Robert Wade and John Logan",
-				"Composer":"Thomas Newman",
-				"Bond":"Daniel Craig",
-				"Budget":"$200,000,000.00",
-				"BoxOffice":"$879,620,923.00",
-				"Image":"spectre.jpg"
+        			{
+				"Title": The Midnight Library,
+				"Author":"Matt Haig",
+				"Year":"2020",
+				"Pages": "304",
+        "Stars": 4.7
+				"Image":"1italy.jpg"
+			  },
+        			{
+				"Title": Things Fall Apart,
+				"Author":"Chinua Achebe",
+				"Year":"1958",
+				"Pages": "224",
+        "Stars": 4.5,
+				"Image":"Uruagauy.jpg"
+			  },
+				      {
+				"Title": Twilight,
+				"Author":"Stephanie Meyer",
+				"Year":"2005",
+				"Pages": "498",
+        "Stars": 4.2,
+				"Image":"1970brazil.jpg"
 			  },
         
+			        {
+				"Title":All the Light We Cannot See,
+				"Author":"Anthony Doer",
+				"Year":"2014",
+				"Pages": "544",
+        "Stars": 4.1,
+				"Image":"england1966.jpg"
+			  },
               {
-				"Film":22,
-				"Title":"Quantum of Solace",
-				"Year":2008,
-				"Director":"Marc Forster",
-				"Producers":"Michael G. Wilson and Barbara Broccoli",
-				"Writers":"Neal Purvis, Robert Wade and Paul Haggis",
-				"Composer":"David Arnold",
-				"Bond":"Daniel Craig",
-				"Budget":"$102,000,000.00",
-				"BoxOffice":"$594,420,283.00",
-				"Image":"quantum-of-solace.jpg"
+				"Title":The Book Theif,
+				"Author":"Markus Zusak",
+				"Year":"2005",
+				"Pages": "584",
+        "Stars": 4,
+				"Image":"1westger.jpg"
 			  },
-			  {
-				"Film":21,
-				"Title":"Casino Royale",
-				"Year":2006,
-				"Director":"Martin Campbell",
-				"Producers":"Michael G. Wilson and Barbara Broccoli",
-				"Writers":"Neal Purvis, Robert Wade and Paul Haggis",
-				"Composer":"David Arnold",
-				"Bond":"Daniel Craig",
-				"Budget":"$142,000,000.00",
-				"BoxOffice":"$431,942,139.00",
-				"Image":"casino-royale.jpg"
+              {
+				"Title": This Time Next Year,
+				"Author":"Sophie Cousens",
+				"Year":"2020",
+				"Pages": "368",
+        "Stars": 3.9,
+				"Image":"1962brazil.jpg"
 			  },
-			  {
-				"Film":20,
-				"Title":"Die Another Day",
-				"Year":2002,
-				"Director":"Lee Tamahori",
-				"Producers":"Michael G. Wilson and Barbara Broccoli",
-				"Writers":"Neal Purvis and Robert Wade",
-				"Composer":"David Arnold",
-				"Bond":"Pierce Brosnan",
-				"Budget":"$135,000,000.00",
-				"BoxOffice":"$361,730,660.00",
-				"Image":"die-another-day.jpg"
+              {
+				"Title": The Careful Undressing of Love,
+				"Author":"Corey Ann Haydu",
+				"Year":"2017",
+				"Pages": "288",
+        "Stars": 3.7,
+				"Image":"1974wg.jpg"
+              
 			  },
-			  {
-				"Film":18,
-				"Title":"Tomorrow Never Dies",
-				"Year":1997,
-				"Director":"Roger Spottiswoode",
-				"Producers":"Michael G. Wilson and Barbara Broccoli",
-				"Writers":"Bruce Feirstein",
-				"Composer":"David Arnold",
-				"Bond":"Pierce Brosnan",
-				"Budget":"$60,000,000.00",
-				"BoxOffice":"$356,429,941.00",
-				"Image":"tomorrow-never-dies.jpg"
+              {
+				"Title": It Ends With Us,
+				"Author":"Colleen Hoover",
+				"Year":"2016",
+				"Pages": "376",
+        "Stars": 3.6,
+				"Image":"Uruagauy.jpg"
 			  },
-			  {
-				"Film":19,
-				"Title":"The World Is Not Enough",
-				"Year":1999,
-				"Director":"Michael Apted",
-				"Producers":"Michael G. Wilson and Barbara Broccoli",
-				"Writers":"Neal Purvis, Robert Wade and Bruce Feirstein",
-				"Composer":"David Arnold",
-				"Bond":"Pierce Brosnan",
-				"Budget":"$110,000,000.00",
-				"BoxOffice":"$339,504,276.00",
-				"Image":"the-world-is-not-enough.jpg"
-			  },
-			  {
-				"Film":11,
-				"Title":"Moonraker",
-				"Year":1979,
-				"Director":"Lewis Gilbert",
-				"Producers":"Albert R. Broccoli",
-				"Writers":"Christopher Wood",
-				"Composer":"John Barry",
-				"Bond":"Roger Moore",
-				"Budget":"$31,000,000.00",
-				"BoxOffice":"$210,300,000.00",
-				"Image":"moonraker.jpg"
-			  },
-			  {
-				"Film":12,
-				"Title":"For Your Eyes Only",
-				"Year":1981,
-				"Director":"John Glen",
-				"Producers":"Albert R. Broccoli",
-				"Writers":"Michael G. Wilson and Richard Maibaum",
-				"Composer":"Bill Conti",
-				"Bond":"Roger Moore",
-				"Budget":"$28,000,000.00",
-				"BoxOffice":"$195,300,000.00",
-				"Image":"for-your-eyes-only.jpg"
-			  },
-			  {
-				"Film":16,
-				"Title":"Licence to Kill",
-				"Year":1989,
-				"Director":"John Glen",
-				"Producers":"Albert R. Broccoli & Michael G. Wilson",
-				"Writers":"Michael G. Wilson and Richard Maibaum",
-				"Composer":"Michael Kamen",
-				"Bond":"Timothy Dalton",
-				"Budget":"$40,000,000.00",
-				"BoxOffice":"$191,200,000.00",
-				"Image":"license-to-kill.jpg"
-			  },
-			  {
-				"Film":13,
-				"Title":"Octopussy",
-				"Year":1983,
-				"Director":"John Glen",
-				"Producers":"Albert R. Broccoli",
-				"Writers":"George MacDonald Fraser, Michael G. Wilson and Richard Maibaum",
-				"Composer":"John Barry",
-				"Bond":"Roger Moore",
-				"Budget":"$27,500,000.00",
-				"BoxOffice":"$187,500,000.00",
-				"Image":"octopussy.jpg"
-			  },
-			  {
-				"Film":10,
-				"Title":"The Spy Who Loved Me",
-				"Year":1977,
-				"Director":"Lewis Gilbert",
-				"Producers":"Albert R. Broccoli",
-				"Writers":"Christopher Wood and Richard Maibaum",
-				"Composer":"Marvin Hamlisch",
-				"Bond":"Roger Moore",
-				"Budget":"$14,000,000.00",
-				"BoxOffice":"$185,400,000.00",
-				"Image":"the-spy-who-loved-me.jpg"
-			  },
-			  {
-				"Film":8,
-				"Title":"Live and Let Die",
-				"Year":1973,
-				"Director":"Guy Hamilton",
-				"Producers":"Harry Saltzman & Albert R. Broccoli",
-				"Writers":"Tom Mankiewicz",
-				"Composer":"George Martin",
-				"Bond":"Roger Moore",
-				"Budget":"$7,000,000.00",
-				"BoxOffice":"$161,800,000.00",
-				"Image":"live-and-let-die.jpg"
-			  },
-			  {
-				"Film":14,
-				"Title":"A View to a Kill",
-				"Year":1985,
-				"Director":"John Glen",
-				"Producers":"Albert R. Broccoli & Michael G. Wilson",
-				"Writers":"Michael G. Wilson and Richard Maibaum",
-				"Composer":"John Barry",
-				"Bond":"Roger Moore",
-				"Budget":"$36,000,000.00",
-				"BoxOffice":"$160,000,000.00",
-				"Image":"a-view-to-a-kill.jpg"
-			  },
-			  {
-				"Film":17,
-				"Title":"GoldenEye",
-				"Year":1995,
-				"Director":"Martin Campbell",
-				"Producers":"Michael G. Wilson and Barbara Broccoli",
-				"Writers":"Michael France, Jeffrey Caine, Kevin Wade and Bruce Feirstein",
-				"Composer":"Éric Serra",
-				"Bond":"Pierce Brosnan",
-				"Budget":"$42,000,000.00",
-				"BoxOffice":"$156,167,015.00",
-				"Image":"goldeneye.jpg"
-			  },
-			  {
-				"Film":15,
-				"Title":"The Living Daylights",
-				"Year":1987,
-				"Director":"John Glen",
-				"Producers":"Albert R. Broccoli & Michael G. Wilson",
-				"Writers":"Michael G. Wilson and Richard Maibaum",
-				"Composer":"John Barry",
-				"Bond":"Timothy Dalton",
-				"Budget":"$30,000,000.00",
-				"BoxOffice":"$152,627,960.00",
-				"Image":"the-living-daylights.jpg"
-			  },
-			  {
-				"Film":4,
-				"Title":"Thunderball",
-				"Year":1965,
-				"Director":"Terence Young",
-				"Producers":"Kevin McClory",
-				"Writers":"Richard Maibaum and John Hopkins",
-				"Composer":"John Barry",
-				"Bond":"Sean Connery",
-				"Budget":"$9,000,000.00",
-				"BoxOffice":"$141,200,000.00",
-				"Image":"thunderball.jpg"
-			  },
-			  {
-				"Film":3,
-				"Title":"Goldfinger",
-				"Year":1964,
-				"Director":"Guy Hamilton",
-				"Producers":"Harry Saltzman & Albert R. Broccoli",
-				"Writers":"Richard Maibaum and Paul Dehn",
-				"Composer":"John Barry",
-				"Bond":"Sean Connery",
-				"Budget":"$3,000,000.00",
-				"BoxOffice":"$124,900,000.00",
-				"Image":"goldfinger.jpg"
-			  },
-			  {
-				"Film":7,
-				"Title":"Diamonds Are Forever",
-				"Year":1971,
-				"Director":"Guy Hamilton",
-				"Producers":"Harry Saltzman & Albert R. Broccoli",
-				"Writers":"Richard Maibaum and Tom Mankiewicz",
-				"Composer":"John Barry",
-				"Bond":"Sean Connery",
-				"Budget":"$7,200,000.00",
-				"BoxOffice":"$116,000,000.00",
-				"Image":"diamonds-are-forever.jpg"
-			  },
-			  {
-				"Film":5,
-				"Title":"You Only Live Twice",
-				"Year":1967,
-				"Director":"Lewis Gilbert",
-				"Producers":"Harry Saltzman & Albert R. Broccoli",
-				"Writers":"Roald Dahl",
-				"Composer":"John Barry",
-				"Bond":"Sean Connery",
-				"Budget":"$9,500,000.00",
-				"BoxOffice":"$111,600,000.00",
-				"Image":"you-only-live-twice.jpg"
-			  },
-			  {
-				"Film":9,
-				"Title":"The Man with the Golden Gun",
-				"Year":1974,
-				"Director":"Guy Hamilton",
-				"Producers":"Harry Saltzman & Albert R. Broccoli",
-				"Writers":"Richard Maibaum and Tom Mankiewicz",
-				"Composer":"John Barry",
-				"Bond":"Roger Moore",
-				"Budget":"$7,000,000.00",
-				"BoxOffice":"$97,600,000.00",
-				"Image":"the-man-with-the-golden-gun.jpg"
-			  },
-			  {
-				"Film":6,
-				"Title":"On Her Majesty's Secret Service",
-				"Year":1969,
-				"Director":"Peter R. Hunt",
-				"Producers":"Harry Saltzman & Albert R. Broccoli",
-				"Writers":"Richard Maibaum",
-				"Composer":"John Barry",
-				"Bond":"George Lazenby",
-				"Budget":"$8,000,000.00",
-				"BoxOffice":"$82,000,000.00",
-				"Image":"on-her-majestys-secret-service.jpg"
-			  },
-			  {
-				"Film":2,
-				"Title":"From Russia with Love",
-				"Year":1963,
-				"Director":"Terence Young",
-				"Producers":"Harry Saltzman & Albert R. Broccoli",
-				"Writers":"Richard Maibaum and Johanna Harwood",
-				"Composer":"John Barry",
-				"Bond":"Sean Connery",
-				"Budget":"$2,000,000.00",
-				"BoxOffice":"$78,900,000.00",
-				"Image":"from-russia-with-love.jpg"
-			  },
-			  {
-				"Film":1,
-				"Title":"Dr. No",
-				"Year":1962,
-				"Director":"Terence Young",
-				"Producers":"Harry Saltzman and Albert R. Broccoli",
-				"Writers":"Richard Maibaum, Johanna Harwood and Berkely Mather",
-				"Composer":"Monty Norman",
-				"Bond":"Sean Connery",
-				"Budget":"$1,000,000.00",
-				"BoxOffice":"$59,567,035.00",
-				"Image":"dr-no.jpg"
+              {
+				"Title": In Five Years,
+				"Author":"Rebecca Serle",
+				"Year":"2020",
+				"Pages": "250",
+        "Stars": 3,
+				"Image":"1italy.jpg"
 			  }
 
 			]
 }
+	
--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <html>
 <head>
-<title> Julis Favorite Books</title>
+<title>Julis Favorite Books</title>
 <style>
 	body {font-family:georgia;}
-  </style> 
-<style>
-.book{
+
+  
+  .film{
     border:1px solid #E77DC2;
     border-radius: 5px;
     padding: 5px;
@@ -18,74 +18,96 @@
     right:10px;
     top:10px;
   }
- .pic img{
-	max-width:50px;
+
+  .pic img{
+	  max-width:150px;
   }
+
   
 </style>
 <script src="https://code.jquery.com/jquery-latest.js"></script>
+
 <script type="text/javascript">
-// $(document).ready(function() {  
-// 	$('.category').click(function(e){
-//         e.preventDefault(); 
-// 		cat = $(this).attr("href");  
-// 		alert(cat);  
-// 	});
-// });	
-  
- /////// 
-function bookTemplate(book){
+
+function bookTemplate(film){
+
   return `
-    <div class="book"> 
-      <b>Book</b>: ${book.Book}<br/>
-      <b>Title</b>: ${book.Title}<br/>
-      <b>Author</b>: ${book.Author}<br/>
-      <b>Genre</b>: ${book.Genre}<br/>
-      <b>Rating</b>: ${book.Rating}<br/>
-      <b>Description</b>: ${book.Description}<br/>
-      <div class="pic"><img src="thumbnails/${book.Image}"/></div>
-    </div>
-    `;
+			  <div class="film">
+         <b> Title </b>: ${film.Title}<br />
+         <b> Author </b>: ${film.Author} <br />
+         <b> Year</b>:   ${film.Year} <br />
+         <b> Pages</b>: ${film.Pages}<br />
+         <b> Stars</b>: ${film.Stars}<br />
+        <div class="pic"><img src="images/${film.Image}" /></div>
+        </div>
+  `;
+} 
   
-  }
 $(document).ready(function() { 
  
- $('.category').click(function(e){
-   e.preventDefault(); 
-   cat = $(this).attr("href");  
-   var request = $.ajax({
-     url: "api.php?cat=" + cat,
-     method: "GET",
-     dataType: "json"
-   });
-   request.done(function( data ) {
-     console.log(data);
-    $("#booktitle").html(data.title);
-    $("#books").html("");
-    $.each(data.books,function(i,item){ 
-      let myBook = bookTemplate(item);
-    $("<div></div>").html(myBook).appendTo("#books");  
-     });
- 
-   });
-  request.fail(function(xhr, status, error ) {
-alert('Error - ' + xhr.status + ': ' + xhr.statusText);
-   });
- 
+  $('.category').click(function(e){
+    e.preventDefault(); //stop default action of the link
+    cat = $(this).attr("href");  //get category from URL
+  
+    var request = $.ajax({
+      url: "api.php?cat=" + cat,
+      method: "GET",
+      dataType: "json"
+    });
+    request.done(function( data ) {
+      console.log(data);
+
+      /*
+      //using JSON.stringify we can view the data on the page
+      let myData = JSON.stringify(data,null,4);
+      myData = "<pre>" + myData + "</p>";
+      $('#output').html(myData);
+      */
+
+      //use data.title to show the order of films
+      $("#filmtitle").html(data.title);
+
+      //clear the previous films
+      $("#films").html("");
+
+      //loop through data.films and add to #films div
+      $.each(data.films,function(i,item){
+          let myFilm = bookTemplate(item);
+          $("<div></div>").html(myFilm).appendTo("#films");
+      });
+
+    });
+    request.fail(function(xhr, status, error ) {
+    alert('Error - ' + xhr.status + ': ' + xhr.statusText);
+    });
   });
-});
+}); 
+
 </script>
 </head>
-  
 	<body>
-	  <h1>Favorite Books</h1>
-		  <a href="alpha" class="category"> In alphabetical order</a> 
-      <br/>
-     <a href="rating" class="category">In order of rating</a>
-		<h3 id="booktitle">Title</h3>
-		<div id="books"></div>
-    
-		<div id="output">Results</div>
-    
+	<h1>Julis Favorite Book</h1>
+  <p> fillerr !<p>
+		<a href="year" class="category">Books By Ranking</a><br />
+		<a href="box" class="category">Books By Page Number</a>
+		<h3 id="filmtitle"></h3>
+		<div id="films">
+      <!--
+			<div class="film">
+        <b>Film</b>: 1<br />
+        <b>Tital</b>: Dr. No<br />
+        <b>Year</b>: 1962<br />
+        <b>Director</b>: Terence Young<br />
+        <b>Producers</b>: Harry Saltzman and Albert R. Broccoli<br />
+        <b>Writers</b>: Richard Maibaum, Johanna Harwood and Berkely Mather<br />
+        <b>Composer</b>: Monty Norman<br />
+        <b>Bond</b>: Sean Connery<br />
+        <b>Budget</b>: $1,000,000.00<br />
+        <b>BoxOffice</b>: $59,567,035.00<br />
+        <div class="pic"><img src="thumbnails/dr-no.jpg" /></div>
+      </div>
+      -->
+		</div>
+		<div id="output"> </div> 
 	</body>
 </html>
